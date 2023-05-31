ad99634f864c05e1e91f8c2c100bd903bf32c9d0
arzhalloun <135104962+arzhalloun@users.noreply.github.com>
2023-05-31 11:38:31 +0300

    Add files via upload

---
+++ simple to-do list app/app.js
@@ -0,0 +1,29 @@
+// Simple to-do list app
+
+let todoForm = document.getElementById("todoForm");
+let todoInput = document.getElementById("todoInput");
+let itemList = document.getElementById("itemList");
+
+todoForm.addEventListener("submit", function (e) {
+	e.preventDefault();
+	if (todoInput.value == "") {
+		inputoEmpty();
+		return false;
+	}
+	addItem(todoInput.value);
+});
+
+function inputoEmpty() {
+	console.log('empty');
+}
+
+function addItem(item) {
+	let listItem = `<li>${item} <button onclick="removeItem(this)">x</button>`
+	list.insertAdjacentHTML("afterbegin", listItem)
+	todoInput.value = "";
+	todoInput.focus();
+}
+
+function removeItem(itemToDelete) {
+	itemToDelete.parentElement.remove();
+}
